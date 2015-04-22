e8dcaf27dd7f02e89c73242565ff04c449661d31
Anis JRIBI <anis@gsa-mena.org>
2015-04-22 20:15:34 +0100

    new apk
    new apk

--- Currency/app/build/generated/source/r/release/com/example/anisjr/currency/R.java
+++ Currency/app/build/generated/source/r/release/com/example/anisjr/currency/R.java
@@ -1315,17 +1315,21 @@ containing a value of this type.
         public static final int action_mode_bar=0x7f09002e;
         public static final int action_mode_bar_stub=0x7f09002d;
         public static final int action_mode_close_button=0x7f090020;
-        public static final int action_refresh=0x7f090046;
-        public static final int action_settings=0x7f090047;
+        public static final int action_refresh=0x7f09004c;
+        public static final int action_settings=0x7f09004d;
         public static final int activity_chooser_view_content=0x7f090021;
         public static final int always=0x7f090016;
         public static final int beginning=0x7f090013;
         public static final int checkbox=0x7f090029;
         public static final int collapseActionView=0x7f090017;
-        public static final int container=0x7f09003f;
+        public static final int currency_detail_container=0x7f09003f;
         public static final int decor_content_parent=0x7f09002f;
         public static final int default_activity_button=0x7f090024;
-        public static final int detail_text=0x7f090040;
+        public static final int detail_currencyFrom_textview=0x7f090042;
+        public static final int detail_currencyTO_textview=0x7f090043;
+        public static final int detail_date_textview=0x7f090041;
+        public static final int detail_icon=0x7f090044;
+        public static final int detail_rate_textview=0x7f090045;
         public static final int dialog=0x7f09001b;
         public static final int disableHome=0x7f09000c;
         public static final int dropdown=0x7f09001c;
@@ -1333,6 +1337,7 @@ containing a value of this type.
         public static final int end=0x7f090014;
         public static final int expand_activities_button=0x7f090022;
         public static final int expanded_menu=0x7f090028;
+        public static final int fragment_history=0x7f090040;
         public static final int home=0x7f090004;
         public static final int homeAsUp=0x7f09000d;
         public static final int icon=0x7f090026;
@@ -1340,11 +1345,12 @@ containing a value of this type.
         public static final int image=0x7f090023;
         public static final int listMode=0x7f090009;
         public static final int list_item=0x7f090025;
-        public static final int list_item_currencyFrom_textview=0x7f090043;
-        public static final int list_item_currencyTo_textview=0x7f090044;
-        public static final int list_item_date_textview=0x7f090042;
-        public static final int list_item_historyRate_textview=0x7f090045;
-        public static final int listview_history=0x7f090041;
+        public static final int list_item_currencyFrom_textview=0x7f09004a;
+        public static final int list_item_currencyTo_textview=0x7f09004b;
+        public static final int list_item_date_textview=0x7f090048;
+        public static final int list_item_historyRate_textview=0x7f090049;
+        public static final int list_item_icon=0x7f090047;
+        public static final int listview_history=0x7f090046;
         public static final int middle=0x7f090015;
         public static final int never=0x7f090019;
         public static final int none=0x7f09000e;
@@ -1407,7 +1413,8 @@ containing a value of this type.
         public static final int fragment_detail=0x7f030019;
         public static final int fragment_main=0x7f03001a;
         public static final int list_item_history=0x7f03001b;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001c;
+        public static final int list_item_today=0x7f03001c;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001d;
     }
     public static final class menu {
         public static final int history_fragment=0x7f0d0000;
@@ -1434,12 +1441,14 @@ containing a value of this type.
         public static final int action_refresh=0x7f0b0010;
         public static final int action_settings=0x7f0b0011;
         public static final int app_name=0x7f0b0012;
-        public static final int hello_world=0x7f0b0013;
-        public static final int pref_currencyFrom_default=0x7f0b0014;
-        public static final int pref_currencyFrom_key=0x7f0b0015;
-        public static final int pref_currencyFrom_label=0x7f0b0016;
-        public static final int title_activity_detail=0x7f0b0017;
-        public static final int title_activity_settings=0x7f0b0018;
+        public static final int content_authority=0x7f0b0013;
+        public static final int hello_world=0x7f0b0014;
+        public static final int pref_currencyFrom_default=0x7f0b0015;
+        public static final int pref_currencyFrom_key=0x7f0b0016;
+        public static final int pref_currencyFrom_label=0x7f0b0017;
+        public static final int sync_account_type=0x7f0b0018;
+        public static final int title_activity_detail=0x7f0b0019;
+        public static final int title_activity_settings=0x7f0b001a;
     }
     public static final class style {
         /**  Customize your theme here. 
@@ -1682,7 +1691,9 @@ containing a value of this type.
         public static final int Widget_AppCompat_Toolbar_Button_Navigation=0x7f0c00df;
     }
     public static final class xml {
-        public static final int pref_general=0x7f050000;
+        public static final int authenticator=0x7f050000;
+        public static final int pref_general=0x7f050001;
+        public static final int syncadapter=0x7f050002;
     }
     public static final class styleable {
         /** Attributes that can be used with a ActionBar.
